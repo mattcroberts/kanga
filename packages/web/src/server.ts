daccd5f239c5bc564e1a386fbb19e447f8e81883
Matt Roberts <matt.roberts@primarybid.com>
2025-10-15 15:54:37 +0100

    use pnpm

--- packages/web/src/server.ts
+++ packages/web/src/server.ts
@@ -8,35 +8,37 @@ import {
 import { createRouter } from "./router";
 
 const secretArn = process.env.CLERK_SECRET_ARN;
-const clerkSecretKey = process.env.CLERK_SECRET_KEY;
+const clerkSecretKeyFromEnv = process.env.CLERK_SECRET_KEY;
+
+let clerkSecretKey: string | undefined;
 
 export default defineHandlerCallback(async (event) => {
-	let secret = clerkSecretKey;
-	if (secretArn) {
-		const clerkSecrets = await getSecret<{
-			CLERK_SECRET_KEY: string;
-			VITE_CLERK_PUBLISHABLE_KEY: string;
-		}>(secretArn, {
-			maxAge: 60,
-			transform: "json",
-		});
+	if (!clerkSecretKey) {
+		if (clerkSecretKeyFromEnv) {
+			clerkSecretKey = clerkSecretKeyFromEnv;
+		} else if (secretArn) {
+			const secretFromAws = await getSecret<{
+				CLERK_SECRET_KEY: string;
+				VITE_CLERK_PUBLISHABLE_KEY: string;
+			}>(secretArn, {
+				maxAge: 60,
+				transform: "json",
+			});
 
-		secret = clerkSecrets!.CLERK_SECRET_KEY;
-	} else if (!clerkSecretKey) {
-		throw new Error(
-			"CLERK_SECRET_ARN and CLERK_SECRET_KEY environment variables are not set",
-		);
+			clerkSecretKey = secretFromAws!.CLERK_SECRET_KEY;
+		} else {
+			throw new Error(
+				"CLERK_SECRET_ARN and CLERK_SECRET_KEY environment variables are not set",
+			);
+		}
 	}
 
-	if (!secret) {
-		throw new Error("Failed to retrieve Clerk secrets");
-	}
 	const handlerFactory = createClerkHandler(
 		createStartHandler({
 			createRouter,
 		}),
 		{
-			secretKey: secret,
+			secretKey: clerkSecretKey,
 			publishableKey: process.env.VITE_CLERK_PUBLISHABLE_KEY,
 		},
 	);
